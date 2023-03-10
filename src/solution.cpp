e1466ee536b37356c3b47958e6184caeb05ea793
小林colin <email@sample.com>
2023-03-10 15:21:36 +0800

    add something

---
+++ src/solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Colin on 2023/3/10.
+//
+
+#include "solution.h"
---
+++ src/solution.h
@@ -0,0 +1,14 @@
+//
+// Created by Colin on 2023/3/10.
+//
+
+#ifndef HUAWEI_CODECRAFT_2023_SOLUTION_H
+#define HUAWEI_CODECRAFT_2023_SOLUTION_H
+
+
+class solution {
+
+};
+
+
+#endif //HUAWEI_CODECRAFT_2023_SOLUTION_H
